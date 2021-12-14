abc33ca69ed358d6cb45ffe3e14fcbb780752bf8
InskalBTHBPPT <78188072+InskalBTHBPPT@users.noreply.github.com>
2021-12-14 20:24:49 +0700

    Update main.cpp
    - add comment
    - change variable name for Voltage Input Value

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include <Arduino.h>
+#include <Arduino.h> //You can comment this line if you use Arduino IDE
 
-#define analogInPin 36
+#define analogInPin 36  //ESP32 GPIO 36 (ADC 0)
 
-int sensorValue = 0;  // value read from the pot
+int sensorValue = 0;  // value read from the ADC0
 
 void setup() {
   // initialize serial communication at 115200
@@ -14,17 +14,21 @@ float read_analogvolt()
 {
   // read the analog in value
   sensorValue = analogRead(analogInPin);
-  //Serial.print(String(sensorValue)+"  ");
-
-  //3.328 adalah reference value
-  //4095 adalah resolusi ADC ESP32
+  
+  //3.328 is voltage reference value
+  //4095 is ESP32 ADC resolution
   float temp = (sensorValue*3.328)/4095.0; 
-  //Serial.print(String(temp)+"  ");
-
-  float VSense = temp / (7500.0/(30000.0+7500.0));
-  VSense = VSense*0.9704 + 0.622; //koreksi atau kalibrasi dengan pembacaan Voltmeter
-  //Serial.println (VSense);
-  return VSense;
+  
+  /*
+  Because this voltage sensor is using voltage divider then
+  R1 = 30k
+  R2 = 7.5k
+  */
+  
+  float Vin = temp / (7500.0/(30000.0+7500.0));
+  Vin= Vin*0.9704 + 0.622; //Corection or calibration factor with power supply voltmeter indicator
+  
+  return Vin;
 }
 
 void loop() {
